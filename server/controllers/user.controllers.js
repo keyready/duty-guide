f8e562d8a222697d657f5fee4475107507671a0e
VALIKO666 <k0fanov36@yandex.ru>
2023-02-17 19:13:03 +0300

    .

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -19,11 +19,15 @@ class UserControllers {
             req.session.userId = id
             req.session.authorized = true
 
-            console.log(req.session)
-
-            const selectedUser = await UserModel.findByPk(id, {raw: true})
-
-            return res.cookie('session',req.session.id).status(200).json(selectedUser)
+            const selectedUser = await UserModel.findByPk(id)
+            res.cookie(
+                'session',
+                `${req.session.id}`,
+            {
+                maxAge: 3600 * 24,
+                secure: true
+            })
+            return res.status(200).json(selectedUser)
         } catch (e) {
             console.log(e.message);
             return res.status(500).json(e.message)
