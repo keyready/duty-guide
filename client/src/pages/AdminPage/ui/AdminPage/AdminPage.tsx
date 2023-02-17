b4b8be22bdc1f59170569033c11fd2f3e6cd3c26
keyready <93424230+keyready@users.noreply.github.com>
2023-02-18 01:24:36 +0300

    DG-011: deploy (собранный проект, предупреждение о неполноценности проекта и его авторах на странице админа)

--- client/src/pages/AdminPage/ui/AdminPage/AdminPage.tsx
+++ client/src/pages/AdminPage/ui/AdminPage/AdminPage.tsx
@@ -1,8 +1,9 @@
 import { classNames } from 'shared/lib/classNames/classNames';
-import React, { memo, useEffect } from 'react';
+import React, { memo, useEffect, useState } from 'react';
 import { HDisclosure } from 'shared/UI/HDisclosure/HDisclosure';
 import { CreateUser } from 'pages/AdminPage/ui/CreateUser/CreateUser';
 import { DeleteItem } from 'pages/AdminPage/ui/DeleteItem/DeleteItem';
+import { Button, Modal } from 'react-bootstrap';
 import { CreateTheory } from '../CreateTheory/CreateTheory';
 import { CreateTask } from '../CreateTask/CreateTask';
 import { CreateCategory } from '../CreateCategory/CreateCategory';
@@ -17,12 +18,39 @@ const AdminPage = memo((props: AdminPageProps) => {
         className,
     } = props;
 
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+
     useEffect(() => {
         document.title = 'Админка';
-    });
+        setShow(true);
+    }, []);
 
     return (
         <div className={classNames(classes.AdminPage, {}, [className])}>
+            <Modal
+                show={show}
+                onHide={handleClose}
+                backdrop="static"
+                keyboard={false}
+                centered
+            >
+                <Modal.Header>
+                    <Modal.Title>Предупреждение</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    Версия приложения 1.0.1, валидации полей нет, поэтому убедительная просьба:
+                    во избежание падения сервера ЗАПОЛНЯТЬ ВСЕ ПОЛЯ, иначе все упадет, и придется
+                    вызывать сантехников.
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="danger" onClick={handleClose}>
+                        Понятно.
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+
             <HDisclosure
                 content={[
                     {
